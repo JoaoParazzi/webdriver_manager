ea1fb794a052c8b0aef395b4c9b3cc2be8611ccf
JoaoParazzi <joaopparazzi@gmail.com>
2026-02-26 21:16:02 -0300

    Primeiro Commit para projeto Selenium

---
+++ src/main/java/br/com/joao/parazzi/App.java
@@ -0,0 +1,13 @@
+package br.com.joao.parazzi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
